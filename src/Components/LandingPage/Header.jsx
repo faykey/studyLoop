b558b2370e370209a809e36b55b73d1a2ce07ad2
Divine <divinenation1@gmail.com>
2025-07-15 12:24:09 -0400

    commit

--- src/Components/LandingPage/Header.jsx
+++ src/Components/LandingPage/Header.jsx
@@ -7,8 +7,9 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-green-50/50 border-b border-[hsla(60,100%,50%,0.3)]">
       <nav className="flex justify-between items-center px-4 py-4">
+        
          
-        <div className="md:ml-10 ml-0">
+        <div className="md:ml-10 ml-0 cursor-pointer" onClick={() => navigate("/")}>
           <img
             src={logo}
             alt="StudyLoop Logo"
@@ -17,12 +18,12 @@ const Header = () => {
         </div>
 
         <div className="flex items-center space-x-4 mr-0 md:mr-16">
-         <button
-        onClick={() => navigate("/login")}  
-        className="text-[hsla(55,65%,81%,1)] font-semibold hover:text-yellow-600 text-lg"
-      >
-        Log In
-      </button>
+          <button
+            onClick={() => navigate("/login")}  
+            className="text-[hsla(55,65%,81%,1)] font-semibold hover:text-yellow-600 text-lg"
+          >
+            Log In
+          </button>
 
           <button
             onClick={() => navigate("/signup")}
