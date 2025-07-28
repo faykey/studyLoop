574f1b0b6d94ca5a021ff2adb914f4ced61a3f22
faykey <fikayomicheal4@gmail.com>
2025-07-28 12:11:31 +0100

    Adjusted landing page signed in user are taken directly to dashboard

--- client/src/pages/LandingPage/Landing.jsx
+++ client/src/pages/LandingPage/Landing.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import Header from "../../Components/LandingPage/Header"
-import HeroSection from "../../Components/LandingPage/HeroSection"
-import StudyCard from "../../Components/LandingPage/StudyCard"
-import StudentsChallenges from "../../Components/LandingPage/StudentsChallenges"
-import SuccessFeatures from "../../Components/LandingPage/SuccessFeatures"
-import Testimonials from "../../Components/LandingPage/Testimonials"
-import HowItWorks from "../../Components/LandingPage/HowItWorks"
-import WaitlistCard from "../../Components/LandingPage/WaitlistCard"
-const Home = () => {
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Header from "../../Components/LandingPage/Header";
+import HeroSection from "../../Components/LandingPage/HeroSection";
+import StudyCard from "../../Components/LandingPage/StudyCard";
+import StudentsChallenges from "../../Components/LandingPage/StudentsChallenges";
+import SuccessFeatures from "../../Components/LandingPage/SuccessFeatures";
+import Testimonials from "../../Components/LandingPage/Testimonials";
+import HowItWorks from "../../Components/LandingPage/HowItWorks";
+import WaitlistCard from "../../Components/LandingPage/WaitlistCard";
+
+const Landing = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/home");
+    }
+  }, [navigate]);
+
   return (
     <div>
-        <Header />
-        <HeroSection />
-        <StudyCard />
-        <StudentsChallenges />
-        <SuccessFeatures />
-        <Testimonials />
-        <HowItWorks />
-        <WaitlistCard />
+      <Header />
+      <HeroSection />
+      <StudyCard />
+      <StudentsChallenges />
+      <SuccessFeatures />
+      <Testimonials />
+      <HowItWorks />
+      <WaitlistCard />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Landing;
